d4f0d42126317d5d6dd2bc33639ee084d549b4c5
Geraint White <geraintwhite@gmail.com>
2016-11-03 11:03:27 +0000

    add partial binary tree implementation

---
+++ BinaryTree.py
@@ -0,0 +1,60 @@
+class Node():
+    def __init__(self, key, data):
+        self.left = None
+        self.right = None
+        self.key = key
+        self.data = data
+
+    def __str__(self):
+        return '{} {}'.format(self.key, self.data)
+
+
+class Tree():
+    def __init__(self, root=None):
+        self.root = root
+
+    def insert(self, key, data=None):
+        child = Node(key, data)
+
+        if self.root is None:
+            self.root = child
+        else:
+            self._insert(child, self.root)
+
+    def _insert(self, child, node):
+        if child.key < node.key:
+            if node.left is None:
+                node.left = child
+            else:
+                self._insert(child, node.left)
+        else:
+            if node.right is None:
+                node.right = child
+            else:
+                self._insert(child, node.right)
+
+    def find(self, key):
+        if self.root is None:
+            return None
+        else:
+            return self._find(key, self.root)
+
+    def _find(self, key, node):
+        if key == node.key:
+            return node
+        if key < node.key and node.left is not None:
+            return self._find(key, node.left)
+        if key > node.key and node.right is not None:
+            return self._find(key, node.right)
+
+    def __str__(self):
+        if self.root is not None:
+            return self._print(self.root)
+
+    def _print(self, node):
+        out = ''
+        if node is not None:
+            out += self._print(node.left)
+            out += str(node) + '\n'
+            out += self._print(node.right)
+        return out
