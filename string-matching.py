818a003fba5369afad88d3e9a4bbc3059a7166d1
Geraint White <geraintwhite@gmail.com>
2017-03-01 21:28:50 +0000

    add finite automata string matching

--- string-matching.py
+++ string-matching.py
@@ -36,3 +36,36 @@ def rabin_karp(string, pattern):
             t = (d * (t - ord(string[s]) * h) + ord(string[s+m])) % q
 
     return matches
+
+
+def transition_function(pattern):
+    d = 256
+    m = len(pattern)
+    table = {}
+
+    for q in range(m):
+        for a in range(d):
+            k = min(m, q+1)
+            while not (pattern[:q]+chr(a))[-k:] == pattern[:k]:
+                k -= 1
+            table[q, a] = k
+
+    return table
+
+
+def finite_automota(string, pattern):
+    n = len(string)
+    m = len(pattern)
+
+    table = transition_function(pattern)
+
+    matches = []
+
+    q = 0
+    for s in range(n):
+        q = max(0, table[q, ord(string[s])])
+        if q == m:
+            q = 0
+            matches.append((s-m+1, s+1))
+
+    return matches
